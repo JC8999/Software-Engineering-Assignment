36b6fade8330231d0102158255d1b847156aae57
JC8999 <S276053@uos.ac.uk>
2025-03-16 02:19:59 +0000

    Fixed superusers being automatically added to the clients group

--- users/models.py
+++ users/models.py
@@ -5,20 +5,29 @@ class CustomUser(AbstractUser):
     ROLE_CHOICES = (
         ("Client", "Client"),
         ("Lawyer", "Lawyer"),
+        ("Admin", "Admin"),
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="Client")
     phone_number = models.CharField(max_length=15, unique=True)
 
     def assign_group(self):
-        """Assign the user to the correct group based on role."""
-        group_name = self.role
-        group, created = Group.objects.get_or_create(name=group_name) # Checks if group exists and creates it if not
-        self.groups.add(group)
+        """Assigns users to the correct group based on role, but keeps superusers out of a group by giving them an admin role."""
+        self.groups.clear()  # Resets group assignments to clear superusers
+
+        if self.is_superuser:
+            self.role = "Admin"  # Automatically set superusers as "Admin". This is a role not a group.
+        elif self.role == "Client":
+            client_group, _ = Group.objects.get_or_create(name="Client")
+            self.groups.add(client_group)
+        elif self.role == "Lawyer":
+            lawyer_group, _ = Group.objects.get_or_create(name="Lawyer")
+            self.groups.add(lawyer_group)
 
     def save(self, *args, **kwargs):
         """Override save method to assign user to a group based on role."""
-        super().save(*args, **kwargs)
-        self.assign_group()  # Assign group after saving
+        super().save(*args, **kwargs)  # Save the user first to prevent errors with updating fields
+        self.assign_group() # Assign group after saving
+        super().save(update_fields=["role"])
 
     def __str__(self):
         return self.get_full_name()
