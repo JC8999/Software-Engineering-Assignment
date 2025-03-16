5dfe783c92b609727cb8ca68c8e4b82107c172f0
JC8999 <S276053@uos.ac.uk>
2025-03-16 02:35:09 +0000

    Fixed name and email not being required fields

--- users/models.py
+++ users/models.py
@@ -8,6 +8,9 @@ class CustomUser(AbstractUser):
         ("Admin", "Admin"),
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="Client")
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, unique=True)
 
     def assign_group(self):
